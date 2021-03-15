19a17dcba7acf3ac2fcb12701c1195fc2a98380a
Diegotpereira <diegoteixeirapereira@hotmail.com>
2021-03-15 15:26:46 -0300

    first commit

---
+++ resources/views/coronas/edit.blade.php
@@ -0,0 +1,62 @@
+@extends('coronas.layout')
+
+@section('title', __('Editar (Corona CRUD com Laravel') - i9w3b'))
+
+@push('css')
+
+<style>
+</style>
+@endpush
+@section('content')
+
+<div class="container">
+     <div class="row justify-content-center">
+         <div class="col-md-12">
+              <div class="card">
+                   <div class="card-header">
+                        <div class="d-flex justify-content-between w-100">
+                            <span>@lang('Editar (Corona CRUD com Laravel) - i9w3b')</span>
+                            <a href="{{ url('coronas') }}" class="btn-info btn-sm">
+                                <i class="fa fa-arrow-left"></i> @lang('Voltar')
+                            </a>
+                        </div>
+                   </div>
+                   <div class="card-body">
+                        @if (session ('status'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('status')}}
+                        </div>
+                        @endif
+
+                        {!! Form::open (['acton' => ['CoronaController@update', $coronacase->id] , 'method' => 'PUT'])}
+
+                        <div class="for-group">
+                             {!! Form::label(__('Nome do país:')) !!}
+                             {!! Form::text("country_name", $coronacases->country_name, ["class"=>"form-control", "required"=> "required"]) !!}
+                        </div>
+                        <div class="form-group">
+                        {!! Form::label(__('Total de Mortes:')) !!}
+                        {!! Form::text("cases", $coronacase->cases, ["id" => "cases", "class"=>"form-control mmss", "required"=> "required"])!!}
+                        </div>
+                        <div class="well well-sm clearfix">
+                             <button class="btn btn-success pull-rigth" title="@lang('Salvar')" type="submit">@lang('Adicionar')</button>
+                        </div>
+                        {!! Form:close() !!}
+                   </div>
+              </div>
+         </div>
+     </div>
+</div>
+@endsection
+
+@push('js')
+<script language='JavaScript'>
+     $(".mmss").focusout(function(){
+         var id = $(this).attr('id');
+         var vall = $(this).val();
+         var regex = /[^0-9]/gm;
+         const result = vall.replace(regex, ``);
+         $('#' + id).val(result);
+     });
+</script>
+@endpush
